efabb89b1ec7a102f7ab994add87f306626c8b96
SidW111 <111293278+SidW111@users.noreply.github.com>
2025-02-24 00:37:26 +0530

    changes

--- client/next.config.ts
+++ client/next.config.ts
@@ -1,9 +1,8 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
-  NEXT_PUBLIC_API_BASE_URL: process.env.NEXT_PUBLIC_API_BASE_URL,
-
+/** @type {import('next').NextConfig} */
+const nextConfig: import('next').NextConfig = {
+  env: {
+    NEXT_PUBLIC_API_BASE_URL: process.env.NEXT_PUBLIC_API_BASE_URL,
+  },
 };
 
 export default nextConfig;
