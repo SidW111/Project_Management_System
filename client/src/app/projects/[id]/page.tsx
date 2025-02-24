e0ece96156bc590e977a7b2ddc01fa2d692961ac
SidW111 <111293278+SidW111@users.noreply.github.com>
2025-02-24 21:20:11 +0530

    deploy

--- client/src/app/projects/[id]/page.tsx
+++ client/src/app/projects/[id]/page.tsx
@@ -8,12 +8,13 @@ import Table from "../TableView";
 import ModalNewTask from "@/components/ModalNewTask";
 import TimeLine from "../TimeLineView";
 
-export type paramsType = Promise<{ id: string }>;
 
-const Project = async (props: {
-  params: Promise<{ id: string }>;
-})  => {
-  const { id } = await props.params;
+type Props = {
+  params: { id: string };
+};
+
+const Project = ({ params }: Props) => {
+  const { id } = params;
   const [activeTab, setActiveTab] = useState("Board");
   const [isModalNewTaskOpen, setIsModalNewTaskOpen] = useState(false);
 
