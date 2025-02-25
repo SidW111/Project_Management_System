de88e23c85acfbdddfa96fd0353c5dbed1bc8dd0
SidW111 <111293278+SidW111@users.noreply.github.com>
2025-02-26 00:14:23 +0530

    adding authentication and authorization

--- client/src/state/api.ts
+++ client/src/state/api.ts
@@ -1,4 +1,5 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+import { fetchAuthSession, getCurrentUser } from "aws-amplify/auth";
 
 export interface Project {
   id: number;
@@ -27,7 +28,7 @@ export interface User {
   userId?: number;
   username: string;
   email: string;
-  profilePictureUrl: string;
+  profilePictureUrl?: string;
   cognitoId?: string;
   teamId?: number;
 }
@@ -43,16 +44,16 @@ export interface Attachment {
 export interface Task {
   id: number;
   title: string;
-  description: string;
-  status: Status;
-  priority: Priority;
-  tags: string;
-  startDate: string;
-  dueDate: string;
-  points: number;
+  description?: string;
+  status?: Status;
+  priority?: Priority;
+  tags?: string;
+  startDate?: string;
+  dueDate?: string;
+  points?: number;
   projectId: number;
-  authorUserId: number;
-  assignedUserId: number;
+  authorUserId?: number;
+  assignedUserId?: number;
 
   author?: User;
   assignee?: User;
@@ -73,12 +74,39 @@ export interface Team {
   projectManagerUserId?: number;
 }
 
-
 export const api = createApi({
-  baseQuery: fetchBaseQuery({ baseUrl: process.env.NEXT_PUBLIC_API_BASE_URL }),
+  baseQuery: fetchBaseQuery({
+    baseUrl: process.env.NEXT_PUBLIC_API_BASE_URL,
+    prepareHeaders: async (headers) => {
+      const session = await fetchAuthSession();
+      const { accessToken } = session.tokens ?? {};
+      if (accessToken) {
+        headers.set("Authorization", `Bearer ${accessToken}`);
+      }
+      return headers;
+    },
+  }),
   reducerPath: "api",
   tagTypes: ["Projects", "Tasks", "Users", "Teams"],
   endpoints: (build) => ({
+    getAuthUser: build.query({
+      queryFn: async (_, _queryApi, _extraoptions, fetchWithBQ) => {
+        try {
+          const user = await getCurrentUser();
+          const session = await fetchAuthSession();
+          if (!session) throw new Error("No session found");
+          const { userSub } = session;
+          const { accessToken } = session.tokens ?? {};
+
+          const userDetailsResponse = await fetchWithBQ(`users/${userSub}`);
+          const userDetails = userDetailsResponse.data as User;
+
+          return { data: { user, userSub, userDetails } };
+        } catch (error: any) {
+          return { error: error.message || "Could not fetch user data" };
+        }
+      },
+    }),
     getProjects: build.query<Project[], void>({
       query: () => "projects",
       providesTags: ["Projects"],
@@ -98,11 +126,12 @@ export const api = createApi({
           ? result.map(({ id }) => ({ type: "Tasks" as const, id }))
           : [{ type: "Tasks" as const }],
     }),
-    getTaskByUser: build.query<Task[], number>({
+    getTasksByUser: build.query<Task[], number>({
       query: (userId) => `tasks/user/${userId}`,
-      providesTags: (result,error,userId) => result 
-      ? result.map(({id}) => ({type: "Tasks", id}))
-      : [{type: "Tasks", id:userId}],
+      providesTags: (result, error, userId) =>
+        result
+          ? result.map(({ id }) => ({ type: "Tasks", id }))
+          : [{ type: "Tasks", id: userId }],
     }),
     createTask: build.mutation<Task, Partial<Task>>({
       query: (task) => ({
@@ -124,18 +153,16 @@ export const api = createApi({
     }),
     getUsers: build.query<User[], void>({
       query: () => "users",
-      providesTags: ["Users"]
+      providesTags: ["Users"],
     }),
     getTeams: build.query<Team[], void>({
-      query:() => "teams",
-      providesTags: ["Teams"] 
+      query: () => "teams",
+      providesTags: ["Teams"],
     }),
-    Search: build.query<SearchResults, string>({
-      query: (query) => `search?query=${query}`
-    })
-    
+    search: build.query<SearchResults, string>({
+      query: (query) => `search?query=${query}`,
+    }),
   }),
-  
 });
 
 export const {
@@ -147,5 +174,6 @@ export const {
   useSearchQuery,
   useGetUsersQuery,
   useGetTeamsQuery,
-  useGetTaskByUserQuery
+  useGetTasksByUserQuery,
+  useGetAuthUserQuery,
 } = api;
