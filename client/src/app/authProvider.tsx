c3a5e65fb69afd80c650b3ef7d3cbd4faa3c19cc
SidW111 <111293278+SidW111@users.noreply.github.com>
2025-02-25 23:11:34 +0530

    cognito authentication

---
+++ client/src/app/authProvider.tsx
@@ -0,0 +1,63 @@
+import { Authenticator, Placeholder } from "@aws-amplify/ui-react";
+import { Amplify } from "aws-amplify";
+
+import "@aws-amplify/ui-react/styles.css"
+import { signUp } from "aws-amplify/auth";
+
+Amplify.configure({
+    Auth:{
+        Cognito:{
+            userPoolId: process.env.NEXT_PUBLIC_COGNITO_USER_POOL_ID || "",
+            userPoolClientId: process.env.NEXT_PUBLIC_COGNITO_USER_POOL_CLIENT_ID || "",
+        }
+    }
+})
+
+const formFields = {
+   signUp:{
+    username:{
+        order: 1,
+        placeholder:"Choose your username",
+        label:"Username",
+        inputprops:{required:true},
+    },
+    email:{
+        order: 2,
+        placeholder:"Enter your email",
+        label:"Email",
+        inputprops:{ type:"email",required:true},
+    },
+    password:{
+        order: 3,
+        placeholder:"Enter your password",
+        label:"Password",
+        inputprops:{type:"password",required:true},
+    },
+    confirm_password:{
+        order: 4,
+        placeholder:"Confirm your password",
+        label:"Confirm Password",
+        inputprops:{type:"password",required:true},
+    }
+   }
+}
+
+const AuthProvider = ({ children }: any) => {
+  return (
+    <div className="mt-5">
+      <Authenticator formFields={formFields}>
+        {({ user }: any) =>
+          user ? (
+            <div>{children}</div>
+          ) : (
+            <div>
+              <h1>Please sign in below:</h1>
+            </div>
+          )
+        }
+      </Authenticator>
+    </div>
+  );
+};
+
+export default AuthProvider;
